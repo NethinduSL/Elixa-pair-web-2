92a94b5f1ea911ead1ce44a30480ef03c4399f3f
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2025-01-01 11:56:34 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'eboxdatabase@gmail.com',
-    password: 'Bitx@NJ2023',
+    email: 'pibon46879@myweblaw.com',
+    password: 'ILOVEELIXA',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
